6d536ad2ad1c57ae2b9ce3349e5f75a7a7879244
Rômulo Felipe <68519193+RomFel@users.noreply.github.com>
2025-07-20 22:02:01 +0000

    add titulo

--- ola_mundo.py
+++ ola_mundo.py
@@ -1,4 +1,7 @@
+from titulo import titulo
+
+titulo('cadastro')
 nome = input('Digite seu nome: ')
 idade = int(input('Digite sua idade: '))
 
-print(f'olá, {nome}!, você tem {idade}')
+print(f'olá, {nome}!, você tem {idade} anos')
---
+++ titulo.py
@@ -0,0 +1,8 @@
+def linha():
+    print('-'*20)
+
+
+def titulo(ttl):
+    linha()
+    print(ttl)
+    linha()
